ccbaa26c4490948f84de3eaab30784f81212e8af
Cristian Hernandez <crissstianfhz@gmail.com>
2025-03-05 18:21:44 -0500

    Home page and header heavily improved

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private el: ElementRef) {}
 
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    // Seleccionamos el elemento header de este componente
+    const header = this.el.nativeElement.querySelector('.header');
+    // Si el scroll vertical supera 100px, agregamos la clase "scrolled"
+    if (window.pageYOffset > 100) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  }
 }
