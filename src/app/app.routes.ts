4f2b7271463c51ea6ef5cfaf19dc19d48d693fed
Cristian Hernandez <crissstianfhz@gmail.com>
2025-03-07 18:51:11 -0500

    feat: Price addition and title change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'Home Page' },
-  { path: 'projects', component: ProjectsComponent, title: 'Home Page' },
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: '**', redirectTo: '' },
 ];
