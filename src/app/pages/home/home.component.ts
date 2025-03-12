4d6b0433cb3f4336476674f81e2f273d00dbf0c5
Cristian Hernandez <crissstianfhz@gmail.com>
2025-03-12 17:50:08 -0400

    feat: add translations for home page
    - Add multilingual support for home page texts
    - Update translation files (`lang/en.json`, `lang/es.json`, etc.)
    - Ensure dynamic content adapts to selected language

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent{
 
 }
